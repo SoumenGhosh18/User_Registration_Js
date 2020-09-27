44ffe502a3d303a9ea31fbf5c0a664e49eb606ea
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-27 19:57:43 +0530

    Added: Code to validate the first name

--- Implementation.js
+++ Implementation.js
@@ -1,5 +1,14 @@
 class Implementation {
+    firstNameValidator(name) {
+        let FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
 
+        if (FIRST_NAME_PATTERN.test(name)) {
+            console.log("Thank you its valid");
+        }
+        else {
+            console.log("sorry its invalid");
+        }
+    }
     
 }
 module.exports = new Implementation();
--- UserRegistrationMain.js
+++ UserRegistrationMain.js
@@ -2,7 +2,21 @@ const a = require('./Implementation');
 class main {
     main() {
         console.log("Welcome to User Validation");
-      
+        console.log("1.FirstName 2.LastName 3.Email 4.PhoneNo 5.Password ");
+        var readline = require('readline');
+        var input = readline.createInterface(process.stdin, process.stdout);
+        input.question("Enter Choice :", function (option) {
+            switch (option) {
+                case "1":
+                    input.question("Enter First Name: ", function (firstName) {
+                        a.firstNameValidator(firstName);
+                    })
+                    break;
+                default:
+                    console.log("Enter Valid option");
+                    break;
+            }
+        }) 
     }
 }
 const obj = new main();
